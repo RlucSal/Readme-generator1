f0f5f3a7880f094ab48cd6286094b3147e40a8af
RlucSal <rluc.sal@gmail.com>
2024-01-01 13:28:49 +0000

    added logic to generate a readme trough prompts in comand line

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Readme-generator1
-Create a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package
+# hola que tal
+  ## Description
+  this is a readme generator
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+bla bla bla
+
+## Usage
+bla bla bla bla
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+tell me why tell me why 
+
+## Tests
+hello test me
+
+## Questions
+If you have any questions, feel free to reach out to me:
+- GitHub: rluc.sal(https://github.com/rluc.sal)
+- Email: rluc.sal@gmail.com
+  
--- index.js
+++ index.js
@@ -1,21 +1,76 @@
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description for your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0',],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
 ];
-
 // function to write README file
 function writeToFile(fileName, data) {
+  const markdownContent = generateMarkdown(data);
+  fs.writeFile(fileName, markdownContent, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+    } else {
+      console.log(`Successfully wrote to ${fileName}`);
+    }
+  });
 }
-
 // function to initialize program
-function init() {
-
+async function init() {
+  try {
+    const answers = await inquirer.prompt(questions);
+    const outputFileName = "README.md"; 
+    writeToFile(outputFileName, answers);
+    console.log(`Successfully generated ${outputFileName}`);
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
-
 // function call to initialize program
 init();
